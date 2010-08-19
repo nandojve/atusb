00f28250c551c278a3ea9b4988a6d751ddb20831
Werner Almesberger <werner@openmoko.org>
2010-08-19 01:05:04 -0300

    Added boot loader for C8051F326 (using f326xbase)
    - fw/boot/Makefile: build DFU-capable bootloader from f326xbase
    - fw/boot/config.h: product-specific configuration

---
+++ fw/boot/config.h
@@ -0,0 +1,15 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+/*
+ *  Platform-specific settings for boot.c
+ *
+ * USB_VENDOR = Qi Hardware
+ * USB_PRODUCT = 802.15.4, device 0
+ *                   -- -         -
+ */
+
+#define	USB_VENDOR	0x20b7
+#define	USB_PRODUCT	0x1540
+
+#endif /* !CONFIG_H */
