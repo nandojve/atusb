3533e0463c71c9e0b7cf433cdc44f4dc7311bb67
Werner Almesberger <werner@openmoko.org>
2010-09-08 17:36:47 -0300

    Found out that we can't share MISO and MOSI after all. Redesigned the atusd board.
    - atusd/atusd.sch, atusd/atusd.brd: separate MISO and MOSI and free the
      extra signal we need by no longer connecting SLP_TR to the host
    - atusd/atusd.sch, atusd/atusd.cmp, atusd/atusd.brd: removed reset circuit
      Power-on reset seems to be sufficient.
    - atusd/atusd.brd: improved AVDD ground plane
    - cam/pcb.pl (rot): cut counter-clockwise to reduce burr
    - cam/pcb.pl: advanced origin for 2nd run

--- atusd/cam/pcb.pl
+++ atusd/cam/pcb.pl
@@ -32,7 +32,9 @@ sub rot
 sub cut
 {
     local (*fn) = $_[0];
+
     shift @_;
+    reverse @_;
     if (defined $x) {
 	if ($x == $_[0]+$x0 && $y == $_[1]+$y0) {
 	    shift @_;
@@ -76,7 +78,7 @@ sub one
 $z = -0.8;
 # x: corner offset, compensation for rotation, array position
 # y: corner offet
-&orig(5+40*0, 5+30*0)
+&orig(5+54*1, 5+30*0)
 &one(*same);
-&orig(5+40*0+15+&mil(1340), 5+30*0+&mil(1030));
+&orig(5+54*1+15+&mil(1340), 5+30*0+&mil(1030));
 &one(*rot);
