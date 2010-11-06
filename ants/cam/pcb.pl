3f28b92c4fff20effa67c75c9fa01cb494ad8083
Werner Almesberger <werner@openmoko.org>
2010-11-06 01:18:56 -0300

    CAM process for the antenna test boards.
    - ants/ants.brd: added board dimensions
    - ants/cam/Makefile, ants/cam/pcb.pl, ants/cam/doit: CAM process for the
      "ants" boards

---
+++ ants/cam/pcb.pl
@@ -0,0 +1,60 @@
+#!/usr/bin/perl
+
+$PI = atan2(1, 1)*4;
+
+$d = 25.4/1000*35;
+
+
+sub orig
+{
+    $x0 = $_[0];
+    $y0 = $_[1];
+}
+
+
+sub mil
+{
+    return $_[0]/1000*25.4;
+}
+
+
+sub cut
+{
+    if (defined $x) {
+	if ($x == $_[0]+$x0 && $y == $_[1]+$y0) {
+	    shift @_;
+	    shift @_;
+	} else {
+	    print "\n";
+	}
+    }
+    while (@_) {
+	$x = shift @_;
+	$y = shift @_;
+#	($x, $y) = (-$y, $x);
+	$x += $x0;
+	$y += $y0;
+	print "$x $y $z\n";
+    }
+}
+
+
+sub pcb
+{
+    &cut(
+      &mil(   0), &mil(   0),
+      &mil( 680), &mil(   0),
+      &mil( 680), &mil( 490),
+      &mil(   0), &mil( 490),
+      &mil(   0), &mil(   0));
+}
+
+
+$z = -0.8;	# full thickness of board
+# x: corner offset, compensation for rotation, array position
+# y: corner offet
+
+&orig(20*0+3, 15*2+23);
+
+$r = $d/2-0.1;	# compensate deflection of board
+&pcb;
